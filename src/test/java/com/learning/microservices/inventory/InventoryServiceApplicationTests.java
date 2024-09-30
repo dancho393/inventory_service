fc2b730b62cafc08d2f0a5a1a1d22f6316fc0e02
dancho393 <danez7000@gmail.com>
2024-09-30 21:36:58 +0200

    added test for ExistsInventoryService

--- src/test/java/com/learning/microservices/inventory/InventoryServiceApplicationTests.java
+++ src/test/java/com/learning/microservices/inventory/InventoryServiceApplicationTests.java
@@ -1,15 +1,46 @@
 package com.learning.microservices.inventory;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+
+    @LocalServerPort
+    private Integer port;
+
+
+    @BeforeEach
+    void prepareTest(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port=port;
+    }
+    static {
+        mySQLContainer.start();
+    }
     @Test
-    void contextLoads() {
+    void checkIfInventoryExists() {
+         RestAssured.given()
+                .when()
+                .get("api/inventory?skuCode=iphone_15&quantity=100")
+                .then()
+                .log().all()
+                .statusCode(200)
+                 .body("result", Matchers.equalTo(true));
+
+
     }
 
 }
